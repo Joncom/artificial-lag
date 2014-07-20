1fe78ce3a27701fc68c9c227d21a486dec416dd9
Jonathan Commins <joncom@gmail.com>
2014-07-20 15:56:07 -0700

    Calculate latency and print to console.

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var io = require('socket.io')();
+var ping_start_time;
 io.on('connection', function(socket){
 	console.log('Client connected.');
+
+	ping(socket);
+
+	socket.on('pong', function() {
+
+		// Calculate latency
+		var current_time = new Date().getTime();
+		var latency = current_time - ping_start_time;
+		console.log(latency);
+
+		ping(socket);
+	});
 });
 io.listen(3000);
+
+function ping(socket) {
+	ping_start_time = new Date().getTime();
+	socket.emit('ping');
+}
